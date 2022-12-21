c221b09d9598a62fa488d1374a68ecde7dce955e
Rawenxy <114915003+Rawenxy@users.noreply.github.com>
2022-12-21 11:32:07 +0400

    fix

--- main.py
+++ main.py
@@ -1,24 +1,142 @@
-import parcing_woman
-import parcing
-import schedule
-import time
+import telebot
+from telebot import types
+from flask import Flask, request
+import os
+from pymongo import MongoClient
+import config
 
 
-
-def main():
-    # schedule.every().day.at('11:17').do(make_mans_db)
-    # schedule.every().day.at('19:00').do(make_mans_db)
-    # schedule.every().day.at('11:18').do(make_woman_db)
-    # schedule.every().day.at('19:01').do(make_woman_db)
-    parcing_woman.make_woman_db()
-    parcing_woman.clear_derectory_data_g()
-    time.sleep(4)
-    parcing.make_mans_db()
-    parcing.clear_derectory_data()
+TG_TOKEN = config.TG_TOKEN
+bot = telebot.TeleBot(TG_TOKEN, parse_mode=None)
+APP_URL = f'https://sneakers.herokuapp.com/{TG_TOKEN}'
+server = Flask(__name__)
 
 
-    # while True:
-    #     schedule.run_pending()
+def get_database():
+    CONNECTION_STRING = config.CONNECTION_STRING
+    client = MongoClient(CONNECTION_STRING)
+    return client['firebox_bot']
+
+
+dbname = get_database()
+collection_name = dbname["shoes"]
+collection_name_w = dbname["shoes_w"]
+
+
+@bot.message_handler(commands=['start'])
+def man_or_woman(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    man = types.KeyboardButton('Мужские')
+    woman = types.KeyboardButton('Женские')
+    markup.add(man, woman)
+    bot.send_message(message.chat.id,
+                     'Привет, этот бот подкинет тебе пару классных кросовок по низкой цене!'
+                     'Для справки используй /help', reply_markup=markup)
+
+
+@bot.message_handler(commands=['help'])
+def help(message):
+    bot.send_message(message.chat.id,
+                     'Для того чтоб вернуться в начало используй "/start"'
+                     '2 раза в день обнавляем базу данных! Занимает 1 минутку :)')
+
+
+brands = ['Adidas', 'Nike', 'Reebok', 'Columbia', 'New Balance']
+
+
+gender = {}
+price_selected = {}
+brand_selected = {}
+price = {
+    '2000₽-3000₽': {f'$gt': 2000, f'$lt': 3000},
+    '3000₽-4000₽': {f'$gt': 3000, f'$lt': 4000},
+    '4000₽ и более': {f'$gt': 4000}
+}
+
+prices_categories = list(price.keys())
+
+
+@bot.message_handler(content_types=['text'])
+def sneakers_find(message):
+    try:
+        global price_selected
+        global gender
+        global brand_selected
+        if message.text in ['Мужские', 'Женские']:
+            gender[message.chat.id] = message.text
+
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            adidas = types.KeyboardButton('Adidas')
+            nike = types.KeyboardButton('Nike')
+            reebok = types.KeyboardButton('Reebok')
+            columbia = types.KeyboardButton('Columbia')
+            new_balance = types.KeyboardButton('New Balance')
+            markup.add(adidas, nike, reebok, columbia, new_balance)
+            return bot.send_message(message.chat.id,
+                                    'Выбери бренд кросовок', reply_markup=markup)
+
+        if message.text in brands:
+            brand_selected[message.chat.id] = message.text
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            for key in prices_categories:
+                price1 = types.KeyboardButton(key)
+                markup.add(price1)
+            return bot.send_message(message.chat.id,
+                                    'Выбери ценовую категорию', reply_markup=markup)
+
+        if message.text in prices_categories:
+
+            price_selected[message.chat.id] = message.text
+
+            gender_collection = collection_name if gender[message.chat.id] == 'Мужские' else collection_name_w
+
+            query = {
+                'item_name': {f'$regex': f'^{brand_selected[message.chat.id]}(.*?)'},
+                'new_price': price[price_selected[message.chat.id]]
+            }
+
+
+            for params in gender_collection.find(query):
+                params_name = params['item_name']
+                params_model = params['item_model']
+                params_old_price = params['old_price']
+                params_new_price = params['new_price']
+                params_size = params['item_size']
+                params_href = params['item_href']
+                params_picture = params['item_picture']
+
+                bot.send_message(
+                    message.chat.id,
+                    f'🔥 <b>{params_name}</b>\n'
+                    f'🥾 {params_model}\n'
+                    f'✅ Новая цена: <b>{params_new_price}₽</b>\n'
+                    f'❌ Старая цена: {params_old_price}₽\n'
+                    f'📐 {params_size}\n'
+                    f'<a href="{params_picture}"> </a>'
+                    f'<a href="{params_href}">Ссылка</a>', parse_mode='HTML'
+                )
+    except:
+        bot.send_message(message.chat.id, 'Что-то не так, перезапусти меня с помошью /start')
+
+
+@server.route(f"/{TG_TOKEN}", methods=['POST'])
+def get_message():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "!", 200
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url=APP_URL)
+    return "!", 200
+
 
 if __name__ == '__main__':
-    main()
+    bot.remove_webhook()
+    bot.set_webhook(url=APP_URL)
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+
+
--- parcing.py
+++ parcing.py
@@ -4,6 +4,7 @@ from pymongo import MongoClient
 import config
 import os
 
+import schedule
 
 def get_database():
     CONNECTION_STRING = config.CONNECTION_STRING
@@ -16,31 +17,42 @@ def get_database():
 dbname = get_database()
 collection_name = dbname["shoes"]
 
+dbname = get_database()
+collection_name_w = dbname["shoes_w"]
+
+
+
+# scrap
 base_url = 'https://fireboxclub.com'
-url = 'https://fireboxclub.com/catalogue/sale990?show=1&sGen=1'
+url_m = 'https://fireboxclub.com/catalogue/sale990?show=1&sGen=1'
 url_g = 'https://fireboxclub.com/catalogue/sale990?show=1&sGen=2'
 
-headers= {
+
+headers = {
  'Accept': '*/*',
- 'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:105.0) Gecko/20100101 Firefox/105.0'
+ 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:105.0) Gecko/20100101 Firefox/105.0'
 }
 
-# Сделали запрос, поняли сколько страниц на сайте mans
+def clear_db():
+    collection_name.delete_many({})
+    collection_name_w.delete_many({})
+    return 'Удалили старые данные'
+
+# scrap man
 def make_mans_db():
-    req = requests.get(url, headers=headers)
+    req = requests.get(url_m, headers=headers)
     src = req.text
     soup = BeautifulSoup(src, 'lxml')
     find_lats_pages = soup.find(class_="pag-wrapper").find_all('a')[-2].text
 
     for count in range(1, int(find_lats_pages)+1):
-        req = requests.get(url+f'&page={count}', headers=headers)
+        req = requests.get(url_m+f'&page={count}', headers=headers)
 
         src = req.text
         print(f'[+][{count}/{find_lats_pages}]Собираем пагинацию мужских кросовок')
 
         with open(f'data/index{count}.html', 'w') as file:
             file.write(src)
-
 
     for count in range(1, int(find_lats_pages)+1):
 
@@ -59,7 +71,9 @@ def make_mans_db():
             new_price = item.find(class_='name').find(class_='new_price_prew').text.split(' ')[0]
             item_size = item.find(class_='size').text.strip()
             item_picture = base_url + item.find(class_='photo p1 current').find('img')['src']
-            print(f'\n{item_name}-{item_model}\nold price:{old_price},\nnew price:{new_price}\n{item_size}\n{item_href}\n{item_picture}\n'+'*'*100)
+            print(f'\n{item_name}-{item_model}\nold price:{old_price},\nnew price:{new_price}'
+                  f'\n{item_size}\n{item_href}\n{item_picture}\n'+'*'*100)
+
             shoes = ({
                 "item_picture": item_picture,
                 'item_name': item_name,
@@ -74,6 +88,7 @@ def make_mans_db():
 
     return print('Мужская база данных обнавлена')
 
+
 def clear_derectory_data():
     try:
         dir = './data'
@@ -85,8 +100,82 @@ def clear_derectory_data():
     return print('Файлы из директории /data отчищены')
 
 
+# scrap woman
+
+def make_woman_db():
+    req = requests.get(url_g, headers=headers)
+    src = req.text
+    soup = BeautifulSoup(src)
+    find_lats_pages = soup.find(class_="pag-wrapper").find_all('a')[-2].text
+
+    for count in range(1, int(find_lats_pages)+1):
+        req = requests.get(url_g+f'&page={count}', headers=headers)
+
+        src = req.text
+        print(f'[+][{count}/{find_lats_pages}]Собираем пагинацию женских кросовок')
+
+        with open(f'data_g/index{count}.html', 'w') as file:
+            file.write(src)
+
+    for count in range(1, int(find_lats_pages)+1):
+
+        with open(f'data_g/index{count}.html') as file:
+            src = file.read()
+
+        soup = BeautifulSoup(src, 'lxml')
+
+        find_objects = soup.find(class_='icat w1400').find_all('a')
+        for item in find_objects:
+            item_href = base_url + item.get('href')
+            item_name = item.find(class_='name').find('b').text
+            item_model = item.find(class_='name').find(class_='upp').text
+            old_price = item.find(class_='name').find(class_='old_price_prew').text.split(' ')[0]
+            new_price = item.find(class_='name').find(class_='new_price_prew').text.split(' ')[0]
+            item_size = item.find(class_='size').text.strip()
+            item_picture = base_url + item.find(class_='photo p1 current').find('img')['src']
+            print(f'\n{item_name}-{item_model}\nold price:{old_price}\nnew price:{new_price}\n'
+                  f'{item_size}\n{item_href}\n{item_picture}\n'+'*'*100)
+
+            shoes_w = ({
+                "item_picture": item_picture,
+                'item_name': item_name,
+                'item_model': item_model,
+                'old_price': int(old_price),
+                'new_price': int(new_price),
+                'item_size': item_size,
+                'item_href': item_href,
+            })
+
+            collection_name_w.replace_one({'item_href': shoes_w['item_href']}, shoes_w, upsert=True)
+
+    return print('Женская база данных обнавлена')
 
 
+def clear_derectory_data_g():
+    try:
+        dir = './data_g'
+        for f in os.listdir(dir):
+            os.remove(os.path.join(dir, f))
+    except:
+        print('Не получилось удалить файлы')
+
+    return print('Файлы из директории /data_g отчищены')
+
+if __name__ == '__main__':
+    schedule.every().day.at("05:59").do(clear_db)
+    schedule.every().day.at("06:00").do(make_mans_db)
+    schedule.every().day.at("06:01").do(clear_derectory_data)
+    schedule.every().day.at("06:02").do(make_woman_db)
+    schedule.every().day.at("06:03").do(clear_derectory_data_g)
+    schedule.every().day.at("12:59:55").do(clear_db)
+    schedule.every().day.at("13:00").do(make_mans_db)
+    schedule.every().day.at("13:01").do(clear_derectory_data)
+    schedule.every().day.at("13:02").do(make_woman_db)
+    schedule.every().day.at("13:03").do(clear_derectory_data_g)
+    make_mans_db()
+    make_woman_db()
+    while True:
+        schedule.run_pending()
 
 
 
